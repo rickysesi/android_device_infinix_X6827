3c0418693d4f607890047cb265350a5029d62d69
XDForKayla <128667500+rickysesi@users.noreply.github.com>
2025-01-30 15:43:43 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_x6827.mk
+    $(LOCAL_DIR)/pb_x6827.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_x6827-eng \
-    twrp_x6827-userdebug \
-    twrp_x6827-user
+    pb_x6827-eng \
+    pb_x6827-userdebug \
+    pb_x6827-user
